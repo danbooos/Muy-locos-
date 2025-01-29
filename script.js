e5c09b1a77b844b85d29954105a848b9b0a68ee4
danbooos <91288781+danbooos@users.noreply.github.com>
2025-01-29 23:58:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to Sorteos Muy Locos!');
+    alert('¡Gracias por visitar nuestra página! Explora nuestros productos y participa en los sorteos.');
+});
